656d543ec5972dc900e126ec54eec4f8fc26070b
Gul Rasulzada <115929488+gulyr@users.noreply.github.com>
2024-08-01 11:40:45 +0400

    JS codes added

---
+++ PROJECTS/To Do List/script.js
@@ -0,0 +1,7 @@
+const inputBox = document.getElementById("inputBox");
+const addBtn = document.getElementById("addBtn");
+const todoList = document.getElementById("todoList");
+const addtoDo = () => {
+  const inputText = inputBox.ariaValueMax.trim();
+};
+addBtn.addEventListener("click", addtoDo);
